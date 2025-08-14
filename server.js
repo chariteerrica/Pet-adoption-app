56fe45b4562734d12596d5f9e23850e4aaee0beb
chariteerrica <chariteerrica@gmail.com>
2025-08-13 22:36:45 -0400

    Prepare for Render deployment

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -36,3 +41,10 @@ mongoose.connect(process.env.MONGO_URI)
       console.log('Server running on port 5001');
     });
   });
+
+  //frontend build
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
