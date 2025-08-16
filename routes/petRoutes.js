d45cd16aba68878470b20d54de2fc9b6b9f13472
chariteerrica <chariteerrica@gmail.com>
2025-08-15 22:55:37 -0400

    updated

--- routes/petRoutes.js
+++ routes/petRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getPets, getPetById } = require("../controllers/petController");
+const { getAllPets, getPetById, createPet } = require("../controllers/petController");
 
 // All routes are **relative paths**
-router.get("/", getPets);
+router.get("/", getAllPets);
 router.get("/:id", getPetById);
+router.post("/", createPet);
+
 
 module.exports = router;
