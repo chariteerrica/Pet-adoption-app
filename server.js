5ef60325fdcd8650618984fbb4f5b252c51cc504
chariteerrica <chariteerrica@gmail.com>
2025-08-17 11:38:40 -0400

    changed server

--- server.js
+++ server.js
@@ -15,14 +15,17 @@ app.use("/api/pets", require("./routes/petRoutes"));
 app.use("/api/adoptions", require("./routes/adoptionRoutes"));
 
 
+const path = require('path');
 if (process.env.NODE_ENV === "production") {
   const buildPath = path.join(__dirname, "client", "build");
   app.use(express.static(buildPath));
 
-  app.get("*", (req, res) => {
+  app.get("*", (_, res) => {
     res.sendFile(path.join(buildPath, "index.html"));
   });
 }
+
+
 
 
 // --- Health Check ---
